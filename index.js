708e16671a26edd54f01800edb94eca811a66179
amirn003 <amir.naar@gmail.com>
2023-10-27 18:35:27 +0200

    add toggle open in event listener

---
+++ index.js
@@ -0,0 +1,10 @@
+const panels = document.querySelectorAll('.panel');
+console.log(panels)
+
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', toggleOpen)
+})
